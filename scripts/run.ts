dbfe2796f02623a1b554ffd591a369df2fc99da3
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-11 23:57:13 -0300

    feat(playground): add self waving run

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,10 +1,23 @@
 import {run, ethers} from "hardhat"
 
 const main = async () => {
+    const [owner, randomAddress] = await ethers.getSigners()
     const waveContractFactory = await ethers.getContractFactory("WavePortal")
     const waveContract = await waveContractFactory.deploy()
     await waveContract.deployed()
+
     console.log(`Contract deployed to: ${waveContract.address}`)
+    console.log(`Contract deployed by: ${owner.address}`)
+
+    const waveCountBeforeWaving = await waveContract.getTotalWaves()
+
+    const waveSelfTx = await waveContract.wave()
+    await waveSelfTx.wait()
+
+    const waveCountAfterWaving = await waveContract.getTotalWaves()
+
+    console.log(`WaveCountBeforeWaving: ${waveCountBeforeWaving}`)
+    console.log(`WaveCountAfterWaving: ${waveCountAfterWaving}`)
 }
 
 main()
