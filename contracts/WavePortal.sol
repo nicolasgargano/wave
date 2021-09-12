8ff77a935ee12527bb6e1c60567e230ba1af24e5
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-11 23:40:22 -0300

    feat(contract): add wave counter

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves = totalWaves + 1;
+        console.log("%s waved!", msg.sender);
+    }
+
+    function getTotalWaves() view public returns (uint) {
+        console.log("Total of %d waves", totalWaves);
+        return totalWaves;
     }
 }
