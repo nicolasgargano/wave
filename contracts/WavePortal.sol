9c5cd2d500ecc6b05b6b54d6d0e9681a1bf0a7bc
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-15 03:37:55 -0300

    feat(contract): add reward for waving

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -20,7 +20,7 @@ contract WavePortal {
 
     uint accumulatedGas;
 
-    constructor() {
+    constructor() payable {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
@@ -31,6 +31,11 @@ contract WavePortal {
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint prizeAmount = 0.0001 ether;
+        require(prizeAmount <= address(this).balance, "Trying to withdraw more ether than the contract has.");
+        (bool success,) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
 
         accumulatedGas = accumulatedGas + tx.gasprice;
     }
