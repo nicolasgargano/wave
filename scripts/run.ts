2839fc79b03064ff01537bea196c96076a12dcd6
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-15 03:38:52 -0300

    chore: fund contract in demo run

--- scripts/run.ts
+++ scripts/run.ts
@@ -3,13 +3,16 @@ import {run, ethers} from "hardhat"
 const main = async () => {
     const [owner, randomAddress] = await ethers.getSigners()
     const waveContractFactory = await ethers.getContractFactory("WavePortal")
-    const waveContract = await waveContractFactory.deploy()
+    const waveContract = await waveContractFactory.deploy({value: ethers.utils.parseEther("0.01")})
     await waveContract.deployed()
 
     console.log(`Contract deployed to: ${waveContract.address}`)
     console.log(`Contract deployed by: ${owner.address}`)
 
     const waveCountBeforeWaving = await waveContract.getTotalWaves()
+
+    const contractBalance = await ethers.provider.getBalance(waveContract.address)
+    console.log(`Contract balance: ${ethers.utils.formatEther(contractBalance)}`)
 
     const waveSelfTx = await waveContract.wave("A message!")
     await waveSelfTx.wait()
@@ -19,9 +22,11 @@ const main = async () => {
 
     const waveCountAfterWaving = await waveContract.getTotalWaves()
     const allWaves = await waveContract.getAllWaves()
+    const contractBalanceAfterWaves = await ethers.provider.getBalance(waveContract.address)
 
     console.log(`WaveCountBeforeWaving: ${waveCountBeforeWaving}`)
     console.log(`WaveCountAfterWaving: ${waveCountAfterWaving}`)
+    console.log(`Contract balance after waves: ${contractBalanceAfterWaves}`)
     console.table(allWaves)
 }
 
