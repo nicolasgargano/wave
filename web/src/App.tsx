07d7dc94e901621e2fab8ccb471a52b0cdb5b35c
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-15 01:41:20 -0300

    feat(web): wave and wave count

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from "react"
 import {useEffect} from "react"
+import {WavePortal__factory} from "../../typechain"
+import {ethers} from "ethers"
 
 export const App = () => {
     const [currentAccount, setCurrentAccount] = useState<string>()
+    const [totalWaves, setTotalWaves] = useState<number>()
 
     const checkIfWalletIsConnected = async () => {
         // @ts-ignore
@@ -30,36 +33,46 @@ export const App = () => {
 
     useEffect(() => {
         checkIfWalletIsConnected()
+        updateTotalWaves()
     }, [])
 
-    const wave = () => {
-        console.log("wave!")
+    const updateTotalWaves = async () => {
+        //@ts-ignore
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const contract = WavePortal__factory.connect(import.meta.env.VITE_CONTRACT_ADDRESS, provider)
+
+        const res = await contract.getTotalWaves()
+        console.log(res)
+        setTotalWaves(res.toNumber())
+    }
+
+    const wave = async () => {
+        //@ts-ignore
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = WavePortal__factory.connect(import.meta.env.VITE_CONTRACT_ADDRESS, signer)
+
+        const waveTx = await contract.wave()
+        console.log("Mining...", waveTx.hash)
+
+        await waveTx.wait()
+        console.log("Mined --", waveTx.hash)
+
+        const count = await contract.getTotalWaves()
     }
 
     return (
         <div>
             <main>
-                <h1>
-                    👋 Hey there!
-                </h1>
-
-                <p>
-                    I'm Nico and I'm learning solidity. Connect your Ethereum wallet
-                    and wave at me!
-                </p>
-
+                <h1>👋 Hey there!</h1>
+                <p>{"I'm Nico and I'm learning solidity. Connect your Ethereum wallet and wave at me!"}</p>
                 {
-                    currentAccount ?
-                        <p>Connected account: {currentAccount}</p>
-                        : <button className="connectAccount" onClick={connectWallet}>
-                            Connect account
-                        </button>
+                    currentAccount
+                        ? <p>Connected account: {currentAccount}</p>
+                        : <button className="connectAccount" onClick={connectWallet}>Connect account</button>
                 }
-
-
-                <button className="waveButton" onClick={wave}>
-                    Wave at Me
-                </button>
+                {totalWaves !== undefined ? <p>Total waves: {totalWaves}</p> : null}
+                <button className="waveButton" onClick={wave}>Wave at Me</button>
             </main>
         </div>
     )
