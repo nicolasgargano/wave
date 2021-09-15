6d83cb511d99c7930b677d45b5f6adb53aa67925
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-15 04:03:31 -0300

    feat(contract): add 15 min wave cooldown

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     mapping(address => uint) public wavesByAddress;
+    mapping(address => uint) public lastWavedAt;
     uint totalWaves;
 
     event NewWave(address indexed from, uint timestamp, string message);
@@ -25,19 +26,27 @@ contract WavePortal {
     }
 
     function wave(string memory _message) public {
+        // Cooldown
+        require(lastWavedAt[msg.sender] + 15 minutes < block.timestamp, "You can only wave once every 15 minutes!");
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        // Count waves
         wavesByAddress[msg.sender] = wavesByAddress[msg.sender] + 1;
         totalWaves = totalWaves + 1;
         console.log("%s waved with message %s", msg.sender, _message);
 
-        waves.push(Wave(msg.sender, _message, block.timestamp));
-        emit NewWave(msg.sender, block.timestamp, _message);
-
+        // Prize
         uint prizeAmount = 0.0001 ether;
         require(prizeAmount <= address(this).balance, "Trying to withdraw more ether than the contract has.");
         (bool success,) = (msg.sender).call{value: prizeAmount}("");
         require(success, "Failed to withdraw money from contract.");
 
+        // Tally gas
         accumulatedGas = accumulatedGas + tx.gasprice;
+
+        // Add wave
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getAllWaves() view public returns (Wave[] memory) {
