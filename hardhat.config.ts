f37fef3dcc599a5aefb3f4d8cc781a75acebfb83
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-17 02:56:17 -0300

    ci: run hardhat compile in vercel build

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,9 +10,11 @@ import { HardhatUserConfig } from "hardhat/config"
 const config: HardhatUserConfig =  {
     solidity: "0.8.4",
     networks: {
+        // TODO these env vars are not necessary for web build, but running hardhat compile asks for them
+        //   for now I fixed it with empty defaults, but it's not ideal.
         rinkeby: {
-            url: process.env.ALCHEMY_KEY,
-            accounts: [process.env.PRIVATE_KEY ?? ""]
+            url: process.env.ALCHEMY_KEY ?? "",
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
         }
     }
 }
