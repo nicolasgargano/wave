b6a20956b09851f4ec471df8e99f3fa81c2c0afb
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-27 20:09:36 -0300

    chore: add windi

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import {defineConfig} from "vite"
 import reactRefresh from "@vitejs/plugin-react-refresh"
 import liveReload from "vite-plugin-live-reload"
+import WindiCSS from "vite-plugin-windicss"
 
 export default defineConfig({
     root: "web",
     plugins: [
         liveReload(["./assets/**/*"]),
-        reactRefresh()
+        reactRefresh(),
+        WindiCSS(),
     ],
 })
--- web/index.tsx
+++ web/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./src/App"
+import "virtual:windi.css"
+import "virtual:windi-devtools"
 
 ReactDOM.render(
     <React.StrictMode>
