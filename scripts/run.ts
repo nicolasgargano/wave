860a4350d330a2c5474dd6b7cd9df69c28cc82fd
Nicolás Gargano <nicolasgargano@gmail.com>
2021-10-04 07:00:27 -0300

    chore(scripts): use new contract features in run script

--- scripts/run.ts
+++ scripts/run.ts
@@ -16,21 +16,30 @@ const main = async () => {
 
     const waveSelfTx = await waveContract.wave("A message!")
     await waveSelfTx.wait()
+    await waitPromise(16000)
+    const waveSelfTx2 = await waveContract.wave("A second message!")
+    await waveSelfTx2.wait()
+    await waitPromise(16000)
 
     const waveRandomTx = await waveContract.connect(randomAddress).wave("Another message!")
     await waveRandomTx.wait()
 
     const waveCountAfterWaving = await waveContract.getTotalWaves()
     const allWaves = await waveContract.getAllWaves()
+    const wavesBySelf = await waveContract.getWavesFromAddress(owner.address)
     const contractBalanceAfterWaves = await ethers.provider.getBalance(waveContract.address)
 
     console.log(`WaveCountBeforeWaving: ${waveCountBeforeWaving}`)
     console.log(`WaveCountAfterWaving: ${waveCountAfterWaving}`)
     console.log(`Contract balance after waves: ${contractBalanceAfterWaves}`)
     console.table(allWaves)
+    console.table(wavesBySelf)
 
-    const waveSelfTx2 = await waveContract.wave("A message!")
-    await waveSelfTx2.wait()
+    const waveSelfTx3 = await waveContract.wave("A fourth message!")
+    await waveSelfTx3.wait()
+
+    const wavesBySelf2 = await waveContract.getWavesFromAddress(owner.address)
+    console.table(wavesBySelf2)
 }
 
 main()
@@ -38,4 +47,9 @@ main()
     .catch(err => {
         console.error(err)
         process.exit(1)
+    })
+
+const waitPromise = (millis: number) =>
+    new Promise<null>((resolve, _) => {
+        setTimeout(() => resolve(null), millis)
     })
