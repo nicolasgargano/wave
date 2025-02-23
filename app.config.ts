30865a422887df0b4776a93957cb5e60d24e2bb1
Nicolás Gargano <nicolasgargano@gmail.com>
2025-02-23 17:39:26 -0300

    configure vercel preset

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import tsConfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  server: {
+    preset: "vercel",
+  },
   vite: {
     plugins: [
       tsConfigPaths({
