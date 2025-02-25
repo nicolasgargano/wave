5a8c459bbbc6315400fc77694bc2cd6359a210e3
Nicolás Gargano <nicolasgargano@gmail.com>
2025-02-25 04:42:53 -0300

    select-none on links overlay

--- app/components/LinksOverlay.tsx
+++ app/components/LinksOverlay.tsx
@@ -2,7 +2,7 @@ import { Html } from "@react-three/drei"
 
 export const LinksOverlay = () => (
   <Html as="div" fullscreen className="grid place-items-end">
-    <div className="flex flex-col space-y-1 pointer-events-auto items-end p-2 text-xs leading-normal text-gray-500">
+    <div className="flex flex-col space-y-1 pointer-events-auto items-end p-2 text-xs leading-normal text-gray-500 select-none">
       <a
         className="hover:underline hover:text-gray-400 font-bold"
         href="https://github.com/nicolasgargano/wave"
